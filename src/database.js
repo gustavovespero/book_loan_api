e2c5229671282b10d8080bf3d67da3a0f13fbecd
Gustavo Victor Vespero Ojeda <gustavo.ojeda@sanesul.ms.gov.br>
2021-10-27 17:07:43 -0400

    feat: added database.js file

---
+++ src/database.js
@@ -0,0 +1,9 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('postgres','postgres','postgres',{
+    dialect: 'postgres',
+    host: '172.99.0.2',
+    port: 5432
+})
+
+module.exports = sequelize;
