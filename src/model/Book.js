3461ea0dc3b1320a9b408ca8e80f276bffc01f26
Gustavo Victor Vespero Ojeda <gustavo.ojeda@sanesul.ms.gov.br>
2021-10-27 17:03:10 -0400

    first commit

---
+++ src/model/Book.js
@@ -0,0 +1,33 @@
+const DataTypes = require('sequelize');
+const database = require('../database');
+const Loan_Book = require('./Loan_Book');
+const Loan = require('./Loan');
+
+const Book = database.define('book',{
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    price: DataTypes.DECIMAL,
+    description: DataTypes.STRING
+})
+
+Book.belongsToMany(Loan,{
+    through: Loan_Book,
+    constraint: true,
+    foreignKey: 'book_id'
+})
+
+Loan.belongsToMany(Book,{
+    through: Loan_Book,
+    constraint: true,
+    foreignKey: 'loan_id'
+})
+
+module.exports = Book;
